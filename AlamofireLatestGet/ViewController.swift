abdb5ead136abebbb5d4b9bcf86e3bc7ccbc396f
nikhilbalne <nikhil.b@ecoihub.com>
2020-05-03 14:53:52 +0530

    Added Code

--- AlamofireLatestGet/ViewController.swift
+++ AlamofireLatestGet/ViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    var postsArray = [PostsModel]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        makePostsServiceCall()
     }
 
+    func makePostsServiceCall(){
+        AF.request("\(baseUrl)posts").response {
+            response in
+            if (response.response?.statusCode == 200){
+                if let data = response.data{
+                    do {
+                        let userResponse = try JSONDecoder().decode([PostsModel].self, from: data)
+                        self.postsArray.append(contentsOf: userResponse)
+                        print("Result:\(userResponse)")
+                    } catch let err {
+                        print("Error:\(err.localizedDescription)")
+                    }
+                }
+            } else {
+                print("StatusCode:\(response.response!.statusCode)")
+            }
+            
+        }
+    }
 
 }
 
---
+++ Constants/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  AlamofireLatestGet
+//
+//  Created by Nikhil Balne on 03/05/20.
+//  Copyright © 2020 Nikhil Balne. All rights reserved.
+//
+
+import Foundation
+
+let baseUrl = "https://jsonplaceholder.typicode.com/"
